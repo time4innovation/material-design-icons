572d9016673616d4a1f67ac6f0d2b20126942718
Oliver Schmidt <oliver.schmidt@time4innovation.de>
2026-05-05 11:50:04 +0200

    - processes adjusted to prepare meta-data and get the svg-files from backend - complete update of fonts and icons

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # Material Design Icons
 
+This fork tried to get all available svg-files.
+
 Latest icon fonts and optimized SVGs for material design icons.
 
 This monorepo is used for publishing material design icons in the following formats:
 
-Package | Description
-:--- | :---
-[`@material-design-icons/font`](https://github.com/marella/material-design-icons/tree/main/font#readme) | Latest icon fonts and CSS for material design icons.
-[`@material-design-icons/svg`](https://github.com/marella/material-design-icons/tree/main/svg#readme) | Latest optimized SVGs for material design icons.
+| Package                                                                                                 | Description                                          |
+| :------------------------------------------------------------------------------------------------------ | :--------------------------------------------------- |
+| [`@material-design-icons/font`](https://github.com/marella/material-design-icons/tree/main/font#readme) | Latest icon fonts and CSS for material design icons. |
+| [`@material-design-icons/svg`](https://github.com/marella/material-design-icons/tree/main/svg#readme)   | Latest optimized SVGs for material design icons.     |
 
 All packages are automatically updated and published using GitHub Actions, so they will always have the latest icons from Google.
--- scripts/utils.js
+++ scripts/utils.js
@@ -6,27 +6,24 @@ import got from 'got';
 /* Network */
 
 export const get = async (url, { agent, raw = false } = {}) => {
-  const response = await got(url, { headers: { 'user-agent': agent } });
-  return raw ? response.rawBody : response.body;
+    const response = await got(url, { headers: { 'user-agent': agent } });
+    return raw ? response.rawBody : response.body;
 };
 
-export const downloadAll = async (
-  downloads,
-  { concurrency, ignoreExisting = false } = {}
-) => {
-  if (ignoreExisting) {
-    downloads = await filter(
-      downloads,
-      async (item) => !(await exists(item[1]))
-    );
-  }
-  await apply(download, downloads, { concurrency });
+export const downloadAll = async (downloads, { concurrency, ignoreExisting = false } = {}) => {
+    if (ignoreExisting) {
+        downloads = await filter(downloads, async (item) => !(await exists(item[1])));
+    }
+    await apply(download, downloads, { concurrency });
 };
 
 const download = async (url, file) => {
-  console.log(`Downloading ${path.relative('', file)}`);
-  const data = await get(url, { raw: true });
-  await fs.writeFile(file, data);
+    console.log(`Downloading ${path.relative('', file)}`);
+
+    try {
+        const data = await get(url, { raw: true });
+        await fs.writeFile(file, data);
+    } catch (error) {}
 };
 
 /* Filesystem */
@@ -35,69 +32,69 @@ const download = async (url, file) => {
  * @see https://github.com/jprichardson/node-fs-extra/blob/master/lib/path-exists/index.js
  */
 export const exists = async (path) =>
-  fs
-    .access(path)
-    .then(() => true)
-    .catch(() => false);
+    fs
+        .access(path)
+        .then(() => true)
+        .catch(() => false);
 
 export const mkdirs = async (dirs) => {
-  await map(dirs, async (dir) => {
-    try {
-      await fs.mkdir(dir);
-      console.log(`Created ${path.relative('', dir)}`);
-    } catch (e) {
-      if (e.code !== 'EEXIST') {
-        throw e;
-      }
-    }
-  });
+    await map(dirs, async (dir) => {
+        try {
+            await fs.mkdir(dir);
+            console.log(`Created ${path.relative('', dir)}`);
+        } catch (e) {
+            if (e.code !== 'EEXIST') {
+                throw e;
+            }
+        }
+    });
 };
 
 export const walk = async (dir, func) => {
-  const stat = await fs.lstat(dir);
-  if (stat.isFile()) {
-    await func(dir);
-    return;
-  }
-  const files = await fs.readdir(dir);
-  await map(files, async (file) => {
-    file = path.resolve(dir, file);
-    await walk(file, func);
-  });
+    const stat = await fs.lstat(dir);
+    if (stat.isFile()) {
+        await func(dir);
+        return;
+    }
+    const files = await fs.readdir(dir);
+    await map(files, async (file) => {
+        file = path.resolve(dir, file);
+        await walk(file, func);
+    });
 };
 
 export const remove = async (paths) => {
-  const deleted = await del(paths);
-  deleted.forEach((p) => {
-    console.log(`Deleted ${path.relative('', p)}`);
-  });
+    const deleted = await del(paths);
+    deleted.forEach((p) => {
+        console.log(`Deleted ${path.relative('', p)}`);
+    });
 };
 
 /* Async */
 
 export const apply = async (func, args, { concurrency = 8 } = {}) => {
-  const results = [];
-  let i = 0;
-  const next = async () => {
-    const j = i++;
-    if (j >= args.length) {
-      return;
-    }
-    results[j] = await func(...args[j]);
-    await next();
-  };
-  await Promise.all(Array.from({ length: concurrency }, next));
-  return results;
+    const results = [];
+    let i = 0;
+    const next = async () => {
+        const j = i++;
+        if (j >= args.length) {
+            return;
+        }
+        results[j] = await func(...args[j]);
+        await next();
+    };
+    await Promise.all(Array.from({ length: concurrency }, next));
+    return results;
 };
 
 export const map = async (values, predicate) => {
-  values = values.map((v, i) => [v, i]);
-  return await apply(predicate, values);
+    values = values.map((v, i) => [v, i]);
+    return await apply(predicate, values);
 };
 
 export const filter = async (values, predicate) => {
-  const results = await map(values, predicate);
-  return values.filter((_, i) => results[i]);
+    const results = await map(values, predicate);
+    return values.filter((_, i) => results[i]);
 };
 
 /* Others */
@@ -105,13 +102,13 @@ export const filter = async (values, predicate) => {
 export const sortMap = (map) => Object.fromEntries(Object.entries(map).sort());
 
 export const assertEquals = (actual, expected, name) => {
-  if (actual !== expected) {
-    throw new Error(`${name} should be '${expected}' not '${actual}'`);
-  }
+    if (actual !== expected) {
+        throw new Error(`${name} should be '${expected}' not '${actual}'`);
+    }
 };
 
 export const assertNotEquals = (actual, expected, name) => {
-  if (actual === expected) {
-    throw new Error(`${name} should not be '${expected}'`);
-  }
+    if (actual === expected) {
+        throw new Error(`${name} should not be '${expected}'`);
+    }
 };
